eb2f3e821cb425ff293d08dd1eb4403fab00a823
romGreen <rom.green@live.biu.ac.il>
2024-06-07 17:48:53 +0300

    rom 7.6 add comments

--- app/src/main/java/com/example/phototube_android/VideoActivity.java
+++ app/src/main/java/com/example/phototube_android/VideoActivity.java
@@ -3,15 +3,23 @@ package com.example.phototube_android;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.phototube_android.entities.UserManager;
+
 public class VideoActivity extends AppCompatActivity {
     private VideoView videoView;
     private TextView videoNameTextView, authorTextView;
+    private EditText commentEditText;
+    private Button submitCommentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +49,34 @@ public class VideoActivity extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
+
+        //comment handle
+        commentEditText = findViewById(R.id.commentEditText);
+        submitCommentButton = findViewById(R.id.submitCommentButton);
+
+        // Assume UserManager has a method isLoggedIn()
+        if (UserManager.getInstance().isLoggedIn()) {
+            commentEditText.setVisibility(View.VISIBLE);
+            submitCommentButton.setVisibility(View.VISIBLE);
+
+            submitCommentButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    postComment(commentEditText.getText().toString());
+                    commentEditText.setText("");  // Clear input after posting
+                }
+            });
+        } else {
+            commentEditText.setVisibility(View.GONE);
+            submitCommentButton.setVisibility(View.GONE);
+        }
     }
+
+    private void postComment(String comment) {
+        // Logic to post comment
+        // Update the comments view or notify the user of successful posting
+        Toast.makeText(this, "Comment posted", Toast.LENGTH_SHORT).show();
+        // You might also want to add this comment to a view that displays all comments
+    }
+
 }
